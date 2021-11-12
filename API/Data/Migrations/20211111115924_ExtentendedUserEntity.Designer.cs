d5cb029a8abd2f5a6a651febc840fe648443635c
Switchering <maksser@outlook.com>
2021-11-12 19:19:38 +0300

    End Of section 8

--- API/Data/Migrations/20211111115924_ExtentendedUserEntity.Designer.cs
+++ API/Data/Migrations/20211111115924_ExtentendedUserEntity.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20211111115924_ExtentendedUserEntity")]
+    partial class ExtentendedUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -47,6 +49,9 @@ namespace API.Data.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<string>("LookingFor")
+                        .HasColumnType("TEXT");
+                        
+                    b.Property<string>("Interests")
                         .HasColumnType("TEXT");
 
                     b.Property<byte[]>("PasswordHash")
